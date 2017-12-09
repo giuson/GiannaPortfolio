d068fc443c4db4315e83abdf52eaf6987a2b323d
Gianna Uson <giannauson@gmail.com>
2017-12-09 14:02:38 -0500

    try

--- src/js/components/JsonImageLoader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class JsonImageLoader extends React.Component {
-
-  render(){
-    return (<img src={require("../../assets/" + this.props.children)} width="700px" />);
-  }
-}
