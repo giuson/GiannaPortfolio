684fed408f3c9333ecb129bb591080a3d14af75d
giuson <gianna_uson@brown.edu>
2017-12-09 11:26:51 -0500

    Update ProfileImage.js

--- src/js/components/ProfileImage.js
+++ src/js/components/ProfileImage.js
@@ -2,7 +2,7 @@ import React from "react";
 export default class ProfileImage extends React.Component {
   render(){
     return (
-      <img src={require('../../assets/giannalogo1.png')}
+      <img src={require('/assets/giannalogo1.png')}
         width={(this.props.width) ? this.props.width : "auto"}
         height={(this.props.height) ? this.props.height : "auto"} />
     );
